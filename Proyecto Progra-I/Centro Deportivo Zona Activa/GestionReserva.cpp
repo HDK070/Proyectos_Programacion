5f03e7f13fd6d10740e551521bd20a7f987cae5d
HDK070 <castroheiner269@gmail.com>
2026-08-17 13:16:58 -0600

    iniciacion de Reserva

---
+++ Proyecto Progra-I/Centro Deportivo Zona Activa/GestionReserva.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "GestionReserva.h"
+using namespace std;
+
--- Proyecto Progra-I/Centro Deportivo Zona Activa/GestionReserva.h
+++ Proyecto Progra-I/Centro Deportivo Zona Activa/GestionReserva.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+#include "Cancha.h"
+#include "clientes.h"
+using namespace std;
+
+class Reserva {
+private:
+	cliente* Cliente;
+	Cancha* cancha;
+public:
+
+};
